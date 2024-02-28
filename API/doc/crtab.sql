a0a8cefaa8bbc4701d7765ba000d99a7ed1bb5a6
eoghks <62344247+eoghks@users.noreply.github.com>
2024-02-28 14:58:16 +0900

    Add files via upload

--- API/doc/crtab.sql
+++ API/doc/crtab.sql
@@ -1,7 +1,35 @@
---drop table ss_plogcoll;
 create table url (
-  urlId SERIAL NOT NULL PRIMARY KEY,
+  urlId Integer NOT NULL PRIMARY KEY,
   url VARCHAR(2048) NOT NULL,
   method INTEGER,
   extension INTEGER
 );
+
+create table header(
+  urlid Integer ,
+  key varchar(1024),
+  value varchar(1024)
+);
+alter table header add constraint header_pk primary key(urlid, key);
+
+create table body(
+  urlid Integer ,
+  key varchar(1024),
+  value varchar(1024),
+  isList Integer
+);
+alter table body add constraint body_pk primary key(urlid, key);
+
+--v2 
+create table parent(
+  urlid Integer PRIMARY KEY,
+  parent varchar(1024)
+);
+
+create table mapping(
+  urlid Integer ,
+  colName varchar(1024),
+  responseName varchar(1024),
+  type Integer
+);
+alter table mapping add constraint mapping_pk primary key(urlid, colName);
